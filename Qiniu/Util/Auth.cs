a99404475bd4739f9f2fc3d9b8f8803c7ab46173
Dennis Mo <staywithmo@163.com>
2017-04-11 17:39:58 +0800

    实现PrivateDownloadUrl方法。在Auth中。

--- Qiniu/Util/Auth.cs
+++ Qiniu/Util/Auth.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Qiniu.Util
 {
@@ -19,5 +20,56 @@ namespace Qiniu.Util
         {
             return mac.Sign(Encoding.UTF8.GetBytes(rawUrl));
         }
+
+        public static string PrivateDownloadUrl(string baseUrl, Mac mac, int seconds = 3600)
+        {
+            return PrivateDownloadUrl(baseUrl, seconds, mac);
+        }
+
+        /// <summary>
+        /// Privates the download URL.
+        /// </summary>
+        /// <returns>The download URL.</returns>
+        /// <param name="baseUrl">Base URL.</param>
+        /// <param name="expires">Expires.</param>
+        public static string PrivateDownloadUrl(string baseUrl, long expires, Mac mac)
+        {
+            return PrivateDownloadUrlWithDeadline(baseUrl, Deadline(expires), mac);
+        }
+
+        public delegate long Clock();
+
+        private static Clock clock = delegate () {
+            return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+        };
+
+        private static long Deadline(long expires)
+        {
+            return clock() + expires;
+        }
+
+
+        private static string PrivateDownloadUrlWithDeadline(string baseUrl, long deadline, Mac mac)
+        {
+            StringBuilder b = new StringBuilder();
+            b.Append(baseUrl);
+            int pos = baseUrl.IndexOf("?");
+            if (pos > 0)
+            {
+                b.Append("&e=");
+            }
+            else
+            {
+                b.Append("?e=");
+            }
+            b.Append(deadline);
+
+            String token = createDownloadToken(b.ToString(), mac);
+            b.Append("&token=");
+            b.Append(token);
+            return b.ToString();
+        }
+
+
     }
 }
